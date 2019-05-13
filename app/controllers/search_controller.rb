09151a80019ad24756ce2071bf25c268bcfec6de
Deonte Cooper <deonte.cooper12@gmail.com>
2019-05-13 11:03:41 -0600

    Added strong params for the station facade

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,12 @@
 class SearchController < ApplicationController
 
   def index
-    @stations = StationFacade.new
+    @stations = StationFacade.new()
+  end
+
+  private
+
+  def station_params
+    params.require(:stations).permit(:access_code, :fuel_type_code, :zip, :state, :access_days_time, :street_address, :station_name)
   end
 end
