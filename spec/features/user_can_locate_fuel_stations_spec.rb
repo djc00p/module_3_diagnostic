8bfcedee73f9561d37dcffa67668b8ee37d3ae91
Deonte Cooper <deonte.cooper12@gmail.com>
2019-05-13 09:40:34 -0600

    Finished writing test now setting up db

--- spec/features/user_can_locate_fuel_stations_spec.rb
+++ spec/features/user_can_locate_fuel_stations_spec.rb
@@ -4,7 +4,6 @@ describe "As a user" do
   # When I visit "/"
   visit "/"
 
-end
 # And I fill in the search form with 80206 (Note: Use the existing search form)
  fill_in: "search",  with: 80206
 
@@ -18,10 +17,24 @@ end
  expect(total_results).to eq(90)
 
 # Then I should see a list of the 15 closest stations within 5 miles sorted by distance
+
   within "#15closestsatations" do
     expect(page.all('li').count).to eq(15)
     expect(page).to have_content("5 miles")
   end
+
 # And the stations should be limited to Electric and Propane
+  expect(stations.fuel_types).to eq("Electric and Propane")
+
 # And the stations should only be public, and not private, planned or temporarily unavailable.
+  expect(stations.availability).to eq("public")
+
 # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+  within ".stations" do
+    expect(page).to have_content("Name: 1")
+    expect(page).to have_content("Address: 1")
+    expect(page).to have_content("Fuel Types: 1")
+    expect(page).to have_content("Distance: 1")
+    expect(page).to have_content("Access Times: 1")
+  end
+end
